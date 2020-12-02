e6397ac4bd6259d0da400c136de219c6606c27b8
ryo.maemura <ryomaemura34@gmail.com>
2020-12-02 11:56:11 +0000

    add favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,9 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  # 投稿をお気に入りしたユーザーを参照する中間テーブル
+  has_many :reverses_of_favorite, class_name: 'Favorite'
+  # 投稿をお気に入りしたユーザーを取得する
+  has_many :favorite_by_users, through: :reverses_of_favorite, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  # お気に入り投稿の中間テーブル
+  has_many :favorites
+  # ユーザーがお気に入りした投稿を取得する
+  has_many :favorite_posts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -25,6 +29,23 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  # 投稿をお気に入りするメソッド
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  # 投稿のお気に入りを解除するメソッド
+  def unfavorite(micropost)
+    unfavorite_micropost = self.favorites.find_by(micropost_id: micropost.id)
+    unfavorite_micropost.destroy if unfavorite_micropost
+  end
+  
+  # お気に入り登録しているかを確認するメソッド
+  # 登録済ならtrue 未登録ならfalse を返す
+  def favorite?(micropost)
+    self.favorite_posts.include?(micropost)
   end
   
   def feed_microposts
